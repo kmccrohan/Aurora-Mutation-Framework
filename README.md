4296c0a3b4c47ef44de91d2d74a780ba9e9a95d5
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2018-04-05 18:20:06 -0700

    Initial attempts

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-# Mutation Framework
-This is a project you can fork and use as a template for writing your mutation for Aurora. It takes care of most of the pesky details in the mutation creating process. Have fun coding :)
-## How to use
-### Fork the project
-First, you will need to fork the project
-### Change the package.json
-In the `package.json`file you will need to change the `name`, `description`, and `author` parts of the file
-### Code your mutation
-The next and final thing you need to do is write the code for your mutation in the `index.js` file! Remember to still follow the steps in the [mutation writing tutorial](https://github.com/rainmaker022/Aurora-Mutation-Tutorial).
+# aurora-mutate-many-column-editor
+Mutation that allows you to have a note with many columns.
---
+++ aurora-mutate-many-column-editor/index.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { Editor } from "draft-js";
+import {
+  deSerialize,
+  serialize,
+  serPreview,
+  getSearchableText,
+  addColumn,
+  removeColumn
+} from "./util";
+import "./style.css";
+
+function manyColumnContentView(ContentView, api) {
+  return class extends React.Component {
+    componentDidUpdate(prevProps) {
+      const contentLoaded =
+        prevProps.isLoadingContent === true &&
+        this.props.isLoadingContent === false &&
+        this.props.note.mutationName === "TwoColumnEditor";
+      if (contentLoaded) {
+        this.finishedLoadingContent();
+      }
+      const simulatedKeyCommand =
+        prevProps.simulatedKeyCommand === null &&
+        this.props.simulatedKeyCommand !== null;
+      if (simulatedKeyCommand) {
+        this.handleSimulatedKeyCommand(
+          this.props.simulatedKeyCommand,
+          this.props.ourEditorState
+        );
+      }
+    }
+
+    finishedLoadingContent = () => {
+      this.props.note.getContent().then(content => {
+        const editorState = deSerialize(content[this.props.note.mutationName]);
+        this.props.onContentLoaded(editorState);
+      });
+    };
+
+    handleSimulatedKeyCommand(command, editorState) {
+      if (command === "add-column") {
+        addColumn(editorState);
+        this.onChange(editorState);
+      } else if (command === "remove-column") {
+        if (removeColumn(editorState)) {
+          this.onChange(editorState);
+        }
+      }
+    }
+
+    onChange = state => {
+      const serializedContent = serialize(editorState);
+      const serializedPreview = serPreview(editorState);
+      const searchableText = getSearchableText(editorState);
+
+      this.props.onChange(
+        editorState,
+        serializedContent,
+        serializedPreview,
+        searchableText
+      );
+    };
+
+    onSingleChange = (state, index) => {
+      const editorState = this.props.ourEditorState;
+      editorState.editors[index] = state;
+      this.onChange(editorState);
+    };
+
+    render() {
+      if (
+        this.props.note &&
+        this.props.note.mutationName === "ManyColumnEditor"
+      ) {
+        const { onChange, ...props } = this.props;
+        const editors = this.props.ourEditorState.editors.map(col, index => {
+          let className = "editor";
+          if (index > 0) {
+            className = "editor not-first-editor";
+          }
+          return (
+            <div className={className}>
+              <Editor
+                className={"editor" + index}
+                editorState={col}
+                onChange={editorState => {
+                  this.onSingleChange(editorState, index);
+                }}
+                placeholder={"Change me!"}
+                {...props}
+              />
+            </div>
+          );
+        });
+        return <div className="editor-container">{editors}</div>;
+      }
+      return <ContentView {...this.props} />;
+    }
+  };
+}
+
+module.exports.mutations = {
+  ContentView: manyColumnContentView
+};
+
+window.toolbar.buttons.push({
+  icon: "➕",
+  command: "add-column",
+  hint: "Add a new column to note",
+  noteType: "ManyColumnEditor"
+});
+window.toolbar.buttons.push({
+  icon: "➖",
+  command: "remove-column",
+  hint: "Remove right-most column in note",
+  noteType: "ManyColumnEditor"
+});
